bee6b922c6290ae28ed1350d8a7cbb9bf514c290
Leepeng <140040227+leepengcode@users.noreply.github.com>
2024-07-08 16:28:34 +0700

    add

--- lib/Model/FinalIndicationModel.dart
+++ lib/Model/FinalIndicationModel.dart
@@ -4,21 +4,31 @@ import 'dart:convert';
 import 'package:book/Model/FinalIndicationBuildingMdel.dart';
 import 'package:book/Model/FinalIndicationLandMdel.dart';
 
+import 'package:http/http.dart' as http;
+
+
 class FinalIndication {
   final String? totallandsizesqm;
   final String? totallandvalue;
   final String? totalbuildingsizesqm;
   final String? totalbuildingvalue;
-  final List<Land>? land;
-  final List<Building>? building;
+
+  List<Land>? land = [];
+  List<Building>? building = [];
+
+
+
 
   FinalIndication({
     this.totallandsizesqm,
     this.totallandvalue,
     this.totalbuildingsizesqm,
     this.totalbuildingvalue,
-    required this.land,
-    required this.building,
+
+    this.land,
+    this.building,
+
+
   });
   factory FinalIndication.fromJson(Map<String, dynamic> json) {
     return FinalIndication(
@@ -39,4 +49,38 @@ class FinalIndication {
       'building': building,
     };
   }
+
+
+  Future InsertFinal(FinalIndication dataFinalIndic) async {
+    var request = http.Request(
+        'POST', Uri.parse('http://192.168.1.31:8000/api/insertfinal'));
+    if (dataFinalIndic.totalbuildingsizesqm == '') {
+      request.body = jsonEncode({
+        'totallandsizesqm': dataFinalIndic.totallandsizesqm,
+        'totallandvalue': dataFinalIndic.totallandvalue,
+        'totalbuildingsizesqm': dataFinalIndic.totallandsizesqm,
+        'totalbuildingvalue': dataFinalIndic.totallandvalue,
+        'land': dataFinalIndic.land!.toList(),
+        // 'building': dataFinalIndic.building,
+      });
+    } else {
+      request.body = jsonEncode({
+        'totallandsizesqm': dataFinalIndic.totallandsizesqm,
+        'totallandvalue': dataFinalIndic.totallandvalue,
+        'totalbuildingsizesqm': dataFinalIndic.totallandsizesqm,
+        'totalbuildingvalue': dataFinalIndic.totallandvalue,
+        'land': dataFinalIndic.land!.toList(),
+        'building': dataFinalIndic.building!.toList(),
+      });
+    }
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    } else {
+      print(response.reasonPhrase);
+    }
+  }
+
 }
